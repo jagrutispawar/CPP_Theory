73fa11cea6f8728653408e3b2df4d490dc2a0ee5
jagrutispawar <jagrutipawar0604@gmail.com>
2024-09-01 23:03:53 +0530

    Add files via upload
    This C++ program performs a basic arithmetic operation of adding two integers. It prompts the user to input two integer values, computes their sum, and then displays the result in the format `first_number + second_number = sum`. The program uses standard input and output streams for interaction with the user.

---
+++ Add two integers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+ using namespace std;
+
+
+int main() {
+
+int First_number, second_number, Sum;
+
+cout << "Enter two integers:";
+
+cin >> First_number >> second_number;
+
+// Sum of two number in stored in Variable sum OF TwoNumbers.
+
+Sum = First_number + second_number;
+
+// prints sum
+
+cout << First_number <<"+"<< second_number << "=" << Sum;
+
+return 0;
+
+}
