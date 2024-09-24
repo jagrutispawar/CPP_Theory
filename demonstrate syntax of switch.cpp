42196da73d7819be1fb85c921e9c2ef774f0efd0
jagrutispawar <jagrutipawar0604@gmail.com>
2024-09-24 22:03:27 +0530

    Add files via upload
    This C++ program uses a switch statement to evaluate a character variable. It checks if the character is 'A', 'B', or 'C' and prints a corresponding message. If the character is none of these, it outputs a default message.

---
+++ demonstrate syntax of switch.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// Driver Code
+int main()
+{
+ // switch variable
+ char x = 'A';
+ // switch statements
+ switch (x) {
+ case 'A':
+ cout << "Choice is A";
+ break;
+ case 'B':
+ cout << "Choice is B";
+ break;
+ case 'C':
+ cout << "Choice is C";
+ break;
+ default:
+ cout << "Choice other than A, B and C";
+ break;
+ }
+ return 0;
+}
